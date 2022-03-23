7bbe9dfb9c9ae83aab6b0ca91322c0a227939383
YoungShper <ivantamaz@gmail.com>
2022-03-23 13:22:28 +0300

    v1.0

---
+++ Watch Sync/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace Watch_Sync
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Синхронизировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(24, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(162, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(210, 123);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Watch sync";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Watch Sync/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Watch_Sync
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string[] ports = SerialPort.GetPortNames();
+            foreach(string s in ports)
+            {
+                comboBox1.Items.Add(s);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = Convert.ToString(DateTime.Now);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SerialPort serialPort = new SerialPort(comboBox1.Text);
+                serialPort.BaudRate = 9600;
+                serialPort.Open();
+                serialPort.WriteLine(Convert.ToString(DateTime.Now.Second));
+                serialPort.WriteLine(Convert.ToString(DateTime.Now.Minute));
+                serialPort.WriteLine(Convert.ToString(DateTime.Now.Hour));
+                serialPort.WriteLine(Convert.ToString(DateTime.Now.Day));
+                serialPort.WriteLine(Convert.ToString(DateTime.Now.Month));
+                serialPort.WriteLine(Convert.ToString(DateTime.Now.Year - 2000));
+                switch (DateTime.Now.DayOfWeek)
+                {
+                    case DayOfWeek.Sunday: serialPort.WriteLine("0"); break;
+                    case DayOfWeek.Monday: serialPort.WriteLine("0"); break;
+                    case DayOfWeek.Tuesday: serialPort.WriteLine("0"); break;
+                    case DayOfWeek.Wednesday: serialPort.WriteLine("0"); break;
+                    case DayOfWeek.Thursday: serialPort.WriteLine("0"); break;
+                    case DayOfWeek.Friday: serialPort.WriteLine("0"); break;
+                    case DayOfWeek.Saturday: serialPort.WriteLine("0"); break;
+                }
+                serialPort.Close();
+                Console.WriteLine(DateTime.Now);
+                MessageBox.Show("Время синхронизировано, проверьте часы");
+            }
+            catch { MessageBox.Show("Ошибка");  }
+        }
+    }
+}
+
+
+
